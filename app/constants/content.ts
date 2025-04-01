607243da5bc326273c0d28ab8703827125a4dae4
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-04-01 14:55:39 +0100

    fix: update heading color and improve description in defaultCode

--- app/constants/content.ts
+++ app/constants/content.ts
@@ -1,8 +1,8 @@
 export const defaultCode: string = `<div class="ai-c d-f fd-c h-1/1 jc-c">
   <div class="mx-4 ta-c">
-    <h1 class="ff-c fs-md tc-black">Try the latest version of Yumma CSS</h1>
+    <h1 class="ff-c fs-md tc-indigo">Try the latest version of Yumma CSS</h1>
     <p class="fs-b tc-lead">
-      A CSS framework for the web with abbreviated styles.
+      The CSS framework with abbreviated styles.
     </p>
   </div>
 </div>`;
