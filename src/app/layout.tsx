edb9094cf63a688da96e5f951543a20cfd93a73f
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-04-23 14:41:26 +0100

    fix: update favicon paths for consistency and simplify structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
   title: "Yumma CSS Play",
   description: "Yumma CSS Play based on Sandpack.",
   icons: {
-    icon: "/favicon/favicon/favicon/favicon.ico",
-    apple: "/favicon/favicon/apple-touch-icon.png",
+    icon: "/favicon/favicon.ico",
+    apple: "/favicon/apple-touch-icon.png",
   },
   openGraph: {
     title: "Yumma CSS Play",
