b2891e3638b70eb3418160b58332cb0cf528f01c
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-04-23 01:10:38 +0100

    fix: update favicon links to use SVG and PNG formats for improved compatibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
         <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
