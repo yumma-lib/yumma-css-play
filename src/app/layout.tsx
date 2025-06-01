fc292064e9f5ad17d21d7e0bd5b0595dfb9bf669
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-06-02 00:14:40 +0100

    feat: add favicon and apple-touch-icon links to layout for improved branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
