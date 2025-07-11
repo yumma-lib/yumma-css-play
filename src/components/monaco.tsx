30b2c98e013b2c2600098c9cfb4829712ccbe2a5
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-07-11 01:32:03 +0100

    refactor: remove unused FormatButton import from MonacoEditor component

--- src/components/monaco.tsx
+++ src/components/monaco.tsx
@@ -3,11 +3,10 @@ import { emmetHTML } from "emmet-monaco-es";
 import { handleMount } from "../themes/midnight";
 import { registerProviders } from "../utils/providers";
 import { setupKeybindings } from "../utils/keybindings";
-import { useActiveCode, SandpackStack, useSandpack, RoundedButton } from "@codesandbox/sandpack-react";
+import { useActiveCode, SandpackStack, useSandpack } from "@codesandbox/sandpack-react";
 import { useRef } from "react";
 import Editor from "@monaco-editor/react";
 import Header from "./header";
-import FormatButton from "./format";
 
 function MonacoEditor() {
   const { code, updateCode } = useActiveCode();
