905a9117f6ff311ddc32bd66206fa76ba63fd01b
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-07-29 17:00:12 +0100

    chore: update Yumma CSS version to 3.0.3 in initialStyles

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -2,6 +2,6 @@ export const initialCode: string = `<div class="bg-indigo-12 d-g h-dvh pi-c">
   <h1 class="ff-c fs-4xl ta-c tc-indigo-1">Hello World!</h1>
 </div>`;
 
-export const initialStyles: string = `@import url("https://cdn.jsdelivr.net/gh/yumma-lib/yumma-css@3.0.0/dist/yumma.css");
+export const initialStyles: string = `@import url("https://cdn.jsdelivr.net/gh/yumma-lib/yumma-css@3.0.3/dist/yumma.css");
 
 /* Add your custom styles here */`;
