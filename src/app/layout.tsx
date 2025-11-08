4caf69f82ac4e4ed2fc183d7fa93c0c91e274770
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-11-08 01:43:51 +0000

    add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "@/styles/out.css";
 
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body style={{ backgroundColor: "#151724;" }}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
