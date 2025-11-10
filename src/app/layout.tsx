f91f4eaef1963cc02237ba23e86005f98e90f972
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-11-10 00:31:42 +0000

    add nuqs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/out.css";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const description = "Yumma CSS Play based on Sandpack.";
 
@@ -27,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning style={{ backgroundColor: "#151724" }}>
-        {children}
+        <NuqsAdapter>{children}</NuqsAdapter>
         <Analytics />
         <SpeedInsights />
       </body>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="mx-4" style={{ backgroundColor: "#1e2039" }}>
-      <Image
-        className="h-auto py-2 sm:mx-auto"
-        height={160}
-        width={80}
-        src="/logotype.png"
-        alt="Yumma CSS Play Logo"
-      />
+    <div style={{ backgroundColor: "#1e2039" }}>
+      <div className="mx-auto">
+        <Image
+          className="h-auto py-2 sm:mx-auto"
+          height={160}
+          width={80}
+          src="/logotype.png"
+          alt="Yumma CSS Play Logo"
+        />
+      </div>
     </div>
   );
 };
