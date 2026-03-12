effb1e58735fe9fe48a9878e2a315dec76447fbc
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-03-12 14:42:49 +0000

    refactor: Wrap Yumma CSS configuration in `defineConfig` and remove the `minify` build option.

--- yumma.config.mjs
+++ yumma.config.mjs
@@ -1,9 +1,9 @@
-/** @type {import('@yummacss/nitro').Config} */
-export default {
+import { defineConfig } from "yummacss"
+
+export default defineConfig({
   source: ["src/**/*.{ts,tsx}"],
   output: "src/styles/out.css",
   buildOptions: {
     reset: true,
-    minify: false,
   },
-};
+});
