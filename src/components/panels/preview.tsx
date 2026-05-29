b8f75421a42f48186fa0cd9ac20781105c046ce8
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-29 13:40:44 +0100

    small changes to initial code

--- src/components/panels/preview.tsx
+++ src/components/panels/preview.tsx
@@ -14,11 +14,6 @@ const Preview = forwardRef<HTMLIFrameElement, PreviewProps>(({ code }, ref) => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/@yummacss/runtime"></script>
-    <style>
-      html, body {
-        background: white;
-      }
-    </style>
   </head>
   <body>${code}</body>
 </html>`,
--- src/constants/code.ts
+++ src/constants/code.ts
@@ -4,35 +4,11 @@ export const initialCode: string = `<div class="d-f fd-c ai-c jc-c min-h-vh px-6
 
     <div class="d-f fd-c g-3">
       <h1 class="c-gray-12 fs-5xl fw-100 ff-c lh-2">
-        Type less.<br>Style more.
+        Edit this page!
       </h1>
-      <p class="max-w-sm c-gray fs-sm lh-5">
-        Yumma CSS, the ergonomic CSS framework.
+      <p class="max-w-sm c-gray fs-md lh-5">
+        You could also follow the design tokens and patterns in our documentation.
       </p>
-    </div>
-
-    <div class="d-f ai-c jc-c g-6 fw-w">
-      <a class="px-5 py-2 bg-indigo-5 c-white fs-sm fw-500 td-none us-none c-p h:bg-indigo-6">
-        Get started
-      </a>
-      <a class="d-f ai-c g-1 c-slate-7 fs-sm fw-500 tuo-4 us-none c-p h:td-u h:c-slate-6">
-        <span>View docs</span>
-      </a>
-    </div>
-
-    <div class="d-f ai-c jc-c g-6 fw-w pt-5 bc-silver-4 btw-1">
-      <div class="d-f ai-c g-2 c-gray">
-        <div class="fs-xs">⚡</div>
-        <span class="fs-xs">Compact by design</span>
-      </div>
-      <div class="d-f ai-c g-2 c-gray">
-        <p class="fs-xs">🎨</p>
-        <span class="fs-xs">21 colors × 13 shades</span>
-      </div>
-      <div class="d-f ai-c g-2 c-gray">
-        <p class="fs-xs">📱</p>
-        <span class="fs-xs">Mobile-first</span>
-      </div>
     </div>
 
   </div>`;
