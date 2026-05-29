ce1b381002a2e4a311afbbfa47ae0b73ddcb7ee2
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-29 11:16:11 +0100

    minor fix

--- yumma.config.mjs
+++ yumma.config.mjs
@@ -9,14 +9,14 @@ export default defineConfig({
   },
   theme: {
     colors: {
-      accent: "#bec6f2",
       "accent-dim": "#9ea5cd",
+      "muted-dim": "#464a6e",
+      "surface-dim": "#21243f",
+      accent: "#bec6f2",
       border: "#31365e",
       muted: "#6b7194",
       page: "#151724",
       surface: "#1e2039",
-      "muted-dim": "#464a6e",
-      "surface-dim": "#21243f",
     },
   },
 });
