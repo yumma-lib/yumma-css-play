6397e9759c3a4c9292b5358767f92b3524d5a81a
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-06-07 14:05:05 +0100

    fix: correct HTML structure in initialCode string

--- src/constants/code.ts
+++ src/constants/code.ts
@@ -1,7 +1,5 @@
 export const initialCode: string = `<div class="d-f fd-c ai-c jc-c min-h-vh px-6 py-20 bg-white">
-
   <div class="d-f fd-c ai-c g-5 max-w-lg ta-c">
-
     <div class="d-f fd-c g-3">
       <h1 class="c-gray-12 fs-5xl fw-100 ff-c lh-2">
         Edit this page!
@@ -10,5 +8,5 @@ export const initialCode: string = `<div class="d-f fd-c ai-c jc-c min-h-vh px-6
         You could also follow the design tokens and patterns in our documentation.
       </p>
     </div>
-
-  </div>`;
+  </div>
+</div>`;
